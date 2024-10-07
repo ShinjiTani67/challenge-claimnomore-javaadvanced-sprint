242430ac20318738898bb9ec304cc30999650109
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-07 19:40:19 -0300

    create dentistmapper

---
+++ challange/src/main/java/mapper/DentistMapper.java
@@ -0,0 +1,11 @@
+package mapper;
+
+import dto.DentistDto;
+import dto.UserDto;
+import model.Dentist;
+import model.User;
+
+public interface DentistMapper {
+    static DentistDto toDto(Dentist project);
+    Dentist toEntity(Dentist projectDTO);
+}
