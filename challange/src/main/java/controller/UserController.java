e0f28a12fd9015897983967953b2da5189c48ced
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-29 10:27:50 -0300

    update user controller

--- challange/src/main/java/controller/UserController.java
+++ challange/src/main/java/controller/UserController.java
@@ -2,14 +2,14 @@ package controller;
 
 import dto.DentistDto;
 import dto.UserDto;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import repository.UserRepository;
 import service.DentistService;
 import service.UserService;
 
@@ -17,36 +17,20 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    private final UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<List<userDto>> getAllProjects() {
-        return ResponseEntity.ok(userService.getAllProjects());
+    public List<User> getAllUser(){
+        return userRepository.findAll();
     }
-
-    @GetMapping("/paged")
-    public Page<UserDto> getAllProjectsPaged(Pageable pageable) {
-        return userService.getAllProjectsPaged(pageable);
-    }
-
-
-    @GetMapping("/{uuid}")
-    public ResponseEntity<UserDto> buscarPorUUID(@PathVariable UUID uuid) {
-        Optional<UserDto> userDto = userService.buscarPorUUID(uuid);
-        return userDto.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
     @PostMapping
-    public ResponseEntity<?> createProject(@RequestBody UserDto) {
-        try {
-            UserDto novoDentist = userService.saveProject(userDto);
-            return new ResponseEntity<>(novoProjeto, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-
-            return new ResponseEntity<>("Usuário já existe.", HttpStatus.CONFLICT);
-        }
-
+    public User createUser(@RequestBody User user) {
+        return (User) userRepository.save(user);
     }
 }
