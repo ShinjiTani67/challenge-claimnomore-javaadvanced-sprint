6e9e4e49e1ec9cadf272a8d18c96c841318d6ccf
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-29 11:03:50 -0300

    update claim

--- challange/src/main/java/service/ClaimService.java
+++ challange/src/main/java/service/ClaimService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Claim;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClaimService   {
+    List<Claim> getAllClaim();
+    Claim createClaim(Claim xlaim);
+    Optional<Claim> getUserById(Long id);
+    Claim updateClaim(long id, Claim claimDetails);
+    void deleteClaim(Long id);
 }
--- challange/src/main/java/service/DentistService.java
+++ challange/src/main/java/service/DentistService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Dentist;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface DentistService {
+    List<Dentist> getAllDentist();
+    Dentist createUser(Dentist dentist);
+    Optional<Dentist> getUserById(Long id);
+    Dentist updateDentist(long id, Dentist dentistDetails);
+    void deleteDentist(Long id);
 }
