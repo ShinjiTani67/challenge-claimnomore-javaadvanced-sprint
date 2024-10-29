67c27c9c9fa7ef5f11073eaa383d6ba83dfe4082
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-29 11:30:31 -0300

    create user service logic

---
+++ challange/src/main/java/com/example/challange/service/logic/UserServiceLogic.java
@@ -0,0 +1,9 @@
+package com.example.challange.service.logic;
+
+
+import org.springframework.stereotype.Service;
+import service.UserService;
+
+@Service
+public class UserServiceLogic implements UserService {
+}
