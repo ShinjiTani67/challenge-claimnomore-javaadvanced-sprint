a08823c8173664b5d4902b476d6aa663e00309f9
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-04 09:16:36 -0300

    update a UserServiceLogic

--- challange/src/main/java/logic/UserServiceLogic.java
+++ challange/src/main/java/logic/UserServiceLogic.java
@@ -1,14 +1,57 @@
 package logic;
 
 
+import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
+import service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class UserServiceLogic {
-    @Autowired
-    private UserRepository userRepository;
+public class UserServiceLogic implements UserService {
 
-    public
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceLogic(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        } else {
+            throw new ResourceNotFoundException("User not found with id " + id);
+        }
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new ResourceNotFoundException("User not found with id " + id);
+        }
+    }
+}
 }
