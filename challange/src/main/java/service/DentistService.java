6216ba1e09dcedd107d866fb61270f707416a833
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-08 15:28:18 -0300

    update dentist service

--- challange/src/main/java/service/DentistService.java
+++ challange/src/main/java/service/DentistService.java
@@ -1,14 +1,24 @@
 package service;
 
+import dto.DentistDto;
+import lombok.AllArgsConstructor;
+import mapper.DentistMapper;
 import model.Dentist;
+import org.springframework.stereotype.Service;
+import repository.DentistRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface DentistService {
-    List<Dentist> getAllDentist();
-    Dentist createUser(Dentist dentist);
-    Optional<Dentist> getUserById(Long id);
-    Dentist updateDentist(long id, Dentist dentistDetails);
-    void deleteDentist(Long id);
+
+@Service
+@AllArgsConstructor
+public class DentistService {
+    private final DentistRepository dentistRepository;
+    private final DentistMapper dentistMapper;
+
+    Optional<DentistDto>buscarUUID(UUID uuid) {
+        return dentistRepository.findByUuid(uuid).map(dentistMapper::toDto);
+    }
 }
