f9e6e133965481f6e57a8672146ca0bd1f7b017e
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-08 15:49:06 -0300

    update enterprise controller

--- challange/src/main/java/controller/EnterpriseController.java
+++ challange/src/main/java/controller/EnterpriseController.java
@@ -2,11 +2,10 @@ package controller;
 
 
 import model.Enterprise;
-import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import repository.EnterpriseRepository;
-import repository.UserRepository;
+
 
 import java.util.List;
 
